5e4e6f389d22c150aa03198a161561573f91eaa2
Abdel-Monaam-Aouini <clemonaam@gmail.com>
2024-06-26 15:42:32 +0100

    fix ui

--- components/NumericKeyboard.tsx
+++ components/NumericKeyboard.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-const NumericKeyboard = ({onPress}: {onPress: (event: string) => void}) => {
-  const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'C', '0', ','];
+const NumericKeyboard = ({
+  setPress,
+}: {
+  setPress: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'C', '0', ''];
+
+  const handleKeyPress = (key: string) => {
+    if (key === 'C') {
+      setPress('0');
+    } else {
+      // Handle number press
+      setPress((prev: string) =>
+        prev === '0' ? key.toString() : prev + key.toString(),
+      );
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -10,7 +25,7 @@ const NumericKeyboard = ({onPress}: {onPress: (event: string) => void}) => {
         <TouchableOpacity
           key={index}
           style={styles.key}
-          onPress={() => key && onPress(key)}
+          onPress={() => handleKeyPress(key)}
           disabled={!key}>
           <Text style={styles.keyText}>{key}</Text>
         </TouchableOpacity>
@@ -26,6 +41,7 @@ const styles = StyleSheet.create({
     width: 280,
     justifyContent: 'center',
     marginTop: 50,
+    margin: 10,
   },
   key: {
     width: 80,
--- src/screens/InputScreen.tsx
+++ src/screens/InputScreen.tsx
@@ -3,7 +3,7 @@ import {View, Text, TextInput, Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../../App';
-import NumericKeyboard from '../../components/KeyboardNumber';
+import NumericKeyboard from '../../components/NumericKeyboard';
 
 type InputScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -14,32 +14,12 @@ const InputScreen = () => {
   const [amount, setAmount] = React.useState('');
   const navigation = useNavigation<InputScreenNavigationProp>();
 
-  const handleKeyPress = (key: string) => {
-    if (key === 'C') {
-      setAmount('0.00');
-    } else if (key === 'CONFIRM') {
-      // Handle confirm action
-      console.log('Confirmed amount:', amount);
-    } else {
-      // Handle number press
-      setAmount(prev =>
-        prev === '0.00' ? key.toString() : prev + key.toString(),
-      );
-    }
-  };
-
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-6xl mb-8">${amount || '0.00'}</Text>
-      <TextInput
-        className="border border-gray-300 rounded w-3/4 text-center text-2xl"
-        keyboardType="numeric"
-        value={amount}
-        onChangeText={setAmount}
-        placeholder="Enter amount"
-      />
-
-      <NumericKeyboard onPress={handleKeyPress} />
+      <Text className="border border-gray-300 rounded-md w-3/4 text-center text-5xl">
+        {amount}
+      </Text>
+      <NumericKeyboard setPress={setAmount} />
       <View className="mt-6">
         <Button
           title="Charge"
